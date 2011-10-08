2ec8bc7d2e047ef722c219834a25d79a535ecf8d
Francisco Ferreira <Francisco@Francisco-Ferreiras-iMac.local>
2011-10-08 09:13:03 -0300

    Initial Commit

---
+++ OpenATParser/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  OpenATParser
+//
+//  Created by Francisco Ferreira on 08/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
